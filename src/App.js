04f09ac383018af72d6274971ffa0ea041f5fe6e
naitik0212 <shahnaitik0212@gmail.com>
2018-07-29 16:42:51 -0700

    form addition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Projects from './Components/Projects'
+import AddProject from './Components/AddProject'
+
 
 class App extends Component {
 
   constructor() {
       super();
       this.state = {
+          projects: [
+
+          ]
+      }
+  }
+
+  componentWillMount() {
+      this.setState({
           projects: [
               {
                   title: 'EatHub- Yelp Visualization',
@@ -19,19 +29,26 @@ class App extends Component {
               {
                   title: 'Image Recognition',
                   category: 'Cloud - AWS'
-              }
-          ]
-      }
+              }]
+      });
   }
 
+    handlesAddProject(project) {
+      // console.log(project);
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({projects: projects});
+    }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to Project Manager</h1>
+            <h1 className="App-title"><hr />Welcome to Project Manager<hr /></h1>
+
         </header>
         <div className="App-intro">
+            <AddProject addProject = {this.handlesAddProject.bind(this)}/>
             <h2>My Project List:</h2>
             <Projects projects = {this.state.projects} />
         </div>
